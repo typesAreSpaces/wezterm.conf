ef2fcd03f5f3064325fde235fe25ebcf17cb606e
Jose Abel Castellanos Joo <jabelcastellanosjoo@unm.edu>
2026-03-31 14:53:54 -0600

    Minor changes.

--- wezterm.lua
+++ wezterm.lua
@@ -13,9 +13,11 @@ end
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
+config.color_scheme = 'DoomOne'
 config.color_scheme = 'Gruvbox Material (Gogh)'
---config.color_scheme = 'DoomOne'
-config.window_background_opacity = 1.0
+config.color_scheme = 'GruvboxDarkHard'
+config.color_scheme = 'GruvboxDark'
+config.window_background_opacity = 1
 config.enable_tab_bar = false
 config.font_size = 18.0
 config.audible_bell = "Disabled"
@@ -25,7 +27,7 @@ config.default_prog = { '/bin/zsh', '-l', '-c', '/opt/homebrew/bin/tmux new-sess
 
 -- The art is a bit too bright and colorful to be useful as a backdrop
 -- for text, so we're going to dim it down to 10% of its normal brightness
-local dimmer = { brightness = 0.1 }
+local dimmer = { brightness = 0.2 }
 
 config.enable_scroll_bar = false
 config.min_scroll_bar_height = '2cell'
@@ -42,89 +44,89 @@ config.keys = {
     --key = "phys:I",
     --mods = "CTRL|SHIFT",
     --action = wezterm.action({ ActivateTabRelative = -1 }),
-  --},
-  --{
-    --key = "phys:I",
-    --mods = "CTRL",
-    --action = wezterm.action({ ActivateTabRelative = 1 }),
-  --},
-  { key = 'Tab',        mods = 'CTRL',       action = act.SendString "\x1b[27;5;9~" },
-  { key = 'Tab',        mods = 'SHIFT|CTRL', action = act.SendString "\x1b[27;6;9~" },
-}
-
---config.background = {
-  ---- This is the deepest/back-most layer. It will be rendered first
-  --{
-    --source = {
-      --File = '/Users/typesarespaces/.config/wezterm/Pictures/Backgrounds/spaceship_bg_1.png',
+    --},
+    --{
+      --key = "phys:I",
+      --mods = "CTRL",
+      --action = wezterm.action({ ActivateTabRelative = 1 }),
       --},
-      ---- The texture tiles vertically but not horizontally.
-      ---- When we repeat it, mirror it so that it appears "more seamless".
-      ---- An alternative to this is to set `width = "100%"` and have
-      ---- it stretch across the display
-      --repeat_x = 'Mirror',
-      --hsb = dimmer,
-      ---- When the viewport scrolls, move this layer 10% of the number of
-      ---- pixels moved by the main viewport. This makes it appear to be
-      ---- further behind the text.
-      --attachment = { Parallax = 0.1 },
+      { key = 'Tab',        mods = 'CTRL',       action = act.SendString "\x1b[27;5;9~" },
+      { key = 'Tab',        mods = 'SHIFT|CTRL', action = act.SendString "\x1b[27;6;9~" },
+    }
+
+    --config.background = {
+      ---- This is the deepest/back-most layer. It will be rendered first
+      --{
+        --source = {
+          --File = '/Users/typesarespaces/.config/wezterm/Pictures/Backgrounds/spaceship_bg_1.png',
+        --},
+        ---- The texture tiles vertically but not horizontally.
+        ---- When we repeat it, mirror it so that it appears "more seamless".
+        ---- An alternative to this is to set `width = "100%"` and have
+        ---- it stretch across the display
+        --repeat_x = 'Mirror',
+        --hsb = dimmer,
+        ---- When the viewport scrolls, move this layer 10% of the number of
+        ---- pixels moved by the main viewport. This makes it appear to be
+        ---- further behind the text.
+        --attachment = { Parallax = 0.1 },
       --},
       ---- Subsequent layers are rendered over the top of each other
       --{
         --source = {
           --File = '/Users/typesarespaces/.config/wezterm/Pictures/Overlays/overlay_1_spines.png',
-          --},
-          --width = '100%',
-          --repeat_x = 'NoRepeat',
+        --},
+        --width = '100%',
+        --repeat_x = 'NoRepeat',
 
-          ---- position the spins starting at the bottom, and repeating every
-          ---- two screens.
-          --vertical_align = 'Bottom',
-          --repeat_y_size = '200%',
-          --hsb = dimmer,
+        ---- position the spins starting at the bottom, and repeating every
+        ---- two screens.
+        --vertical_align = 'Bottom',
+        --repeat_y_size = '200%',
+        --hsb = dimmer,
 
-          ---- The parallax factor is higher than the background layer, so this
-          ---- one will appear to be closer when we scroll
-          --attachment = { Parallax = 0.2 },
-          --},
-          --{
-            --source = {
-              --File = '/Users/typesarespaces/.config/wezterm/Pictures/Overlays/overlay_2_alienball.png',
-              --},
-              --width = '100%',
-              --repeat_x = 'NoRepeat',
+        ---- The parallax factor is higher than the background layer, so this
+        ---- one will appear to be closer when we scroll
+        --attachment = { Parallax = 0.2 },
+      --},
+      --{
+        --source = {
+          --File = '/Users/typesarespaces/.config/wezterm/Pictures/Overlays/overlay_2_alienball.png',
+        --},
+        --width = '100%',
+        --repeat_x = 'NoRepeat',
 
-              ---- start at 10% of the screen and repeat every 2 screens
-              --vertical_offset = '10%',
-              --repeat_y_size = '200%',
-              --hsb = dimmer,
-              --attachment = { Parallax = 0.3 },
-              --},
-              --{
-                --source = {
-                  --File = '/Users/typesarespaces/.config/wezterm/Pictures/Overlays/overlay_3_lobster.png',
-                  --},
-                  --width = '100%',
-                  --repeat_x = 'NoRepeat',
+        ---- start at 10% of the screen and repeat every 2 screens
+        --vertical_offset = '10%',
+        --repeat_y_size = '200%',
+        --hsb = dimmer,
+        --attachment = { Parallax = 0.3 },
+      --},
+      --{
+        --source = {
+          --File = '/Users/typesarespaces/.config/wezterm/Pictures/Overlays/overlay_3_lobster.png',
+        --},
+        --width = '100%',
+        --repeat_x = 'NoRepeat',
 
-                  --vertical_offset = '30%',
-                  --repeat_y_size = '200%',
-                  --hsb = dimmer,
-                  --attachment = { Parallax = 0.4 },
-                  --},
-                  --{
-                    --source = {
-                      --File = '/Users/typesarespaces/.config/wezterm/Pictures/Overlays/overlay_4_spiderlegs.png',
-                      --},
-                      --width = '100%',
-                      --repeat_x = 'NoRepeat',
+        --vertical_offset = '30%',
+        --repeat_y_size = '200%',
+        --hsb = dimmer,
+        --attachment = { Parallax = 0.4 },
+      --},
+      --{
+        --source = {
+          --File = '/Users/typesarespaces/.config/wezterm/Pictures/Overlays/overlay_4_spiderlegs.png',
+        --},
+        --width = '100%',
+        --repeat_x = 'NoRepeat',
 
-                      --vertical_offset = '50%',
-                      --repeat_y_size = '150%',
-                      --hsb = dimmer,
-                      --attachment = { Parallax = 0.5 },
-                      --},
-                      --}
+        --vertical_offset = '50%',
+        --repeat_y_size = '150%',
+        --hsb = dimmer,
+        --attachment = { Parallax = 0.5 },
+      --},
+    --}
 
-                      -- and finally, return the configuration to wezterm
-                      return config
+    -- and finally, return the configuration to wezterm
+    return config
